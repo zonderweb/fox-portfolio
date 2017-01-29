d410ca2133f4a22ba0d41c3e7c2075b2b53eb83e
@Scriptus <zonder2009@gmail.com>
2017-01-29 18:26:54 +0200

    add page to scroll script

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,24 @@
-$(function() {
-// // Magnific popup
-//   $('.img-popup-link').magnificPopup({
-//   type: 'image'
-//   // other options
+// $(function() {
+// // // Magnific popup
+// //   $('.img-popup-link').magnificPopup({
+// //   type: 'image'
+// //   // other options
+// // });
 // });
 
+
+// Page to scrool
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
